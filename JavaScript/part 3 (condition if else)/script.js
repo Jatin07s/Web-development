8208b50924c781964d29a6a94561c5d95a312efe
Jatin Kumar Singh <wavingex777@gmail.com>
2025-11-30 10:47:53 +0530

    updated javaScript

---
+++ JavaScript/part 3 (condition if else)/script.js
@@ -0,0 +1,59 @@
+console.log("Hello World!");
+
+let age = 25;
+let grace = 18;
+
+age 
+
+if (age === 25) {
+    console.log("You can drive");
+}
+
+else{
+    console.log("you cannot drive");
+}
+
+
+
+
+
+console.log("3" == 3);
+// == doule equal to means it compare the value , means ek 3 string mai hai doosra int mai but value same hai so javascript mai yeh true return hoga 
+
+
+console.log("3" === 3);
+// === triple equal to means , yeh compare karega value+data type dono ko check krega , means ab yeh value same hai but data type different hai so output false dega 
+
+
+
+let a = 7;
+let b = 8;
+console.log(a!=b && b!=a);
+
+// &&  = (and) operator hai yeh means left and right dono condition true honi chaiye then oly it will be overall true 
+
+// ||  = (or) operator left ya right wali condition mai koi bhi ek sahi ho , statement will be true 
+
+console.log(a!=b || a==b);
+
+
+
+
+
+// if else-if else ladder - means python ma jaise if elif elif else hota tha waise hi idhar if else-if else hota hai 
+
+
+let c = 1;
+
+if (c==18) {
+    console.log("You can drive");
+}
+else if (c==0) {
+    console.log("Are you kidding me :) ");
+}
+else if (c==1) {
+    console.log("You are baby");
+}
+else {
+    console.log("You cannot drive");
+}
